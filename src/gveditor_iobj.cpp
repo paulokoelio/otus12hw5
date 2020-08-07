1bfdfd7132ef1b72c00331049f03e665b9bc7564
Solverdce <solverdce@gmail.com>
2020-08-07 19:44:03 +0300

    Revert "remove all files for review"
    This reverts commit 8fc63df64ee03af0f44abc18101c6e59dd5256de.

---
+++ src/gveditor_iobj.cpp
@@ -0,0 +1,19 @@
+#include "gveditor_iobj.h"
+#include <iostream>
+
+namespace gvedt
+{
+
+inline std::ostream &operator<<(std::ostream &out, const gvedt::iobj &obj)
+{
+    obj.send(out);
+    return out;
+}
+
+inline std::istream &operator>>(std::istream &in, gvedt::iobj &obj)
+{
+    obj.load(in);
+    return in;
+}
+
+} // namespace gvedt
